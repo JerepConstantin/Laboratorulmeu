6743178ef06427f6c05287945c9a0e8ee79c6d8b
Jerep Constantin <constantin.jerep@elev.cihcahul.md>
2022-03-02 14:51:16 +0200

    Jerep

---
+++ Aplicatie01/Aplicatie01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aplicatie01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test");
+        }
+    }
+}
