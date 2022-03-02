0ad2eef2d0e897887fc4f121dc7aef664d5b1f97
Jerep Constantin <constantin.jerep@elev.cihcahul.md>
2022-03-02 15:00:31 +0200

    Calculator inpartirea

--- Aplicatie01/Aplicatie01/Program.cs
+++ Aplicatie01/Aplicatie01/Program.cs
@@ -6,7 +6,12 @@ namespace Aplicatie01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Test");
+            Console.WriteLine("Calculator");
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int c;
+            c = n1 / n2;
+            Console.WriteLine("Suma este : {0}", c);
         }
     }
 }
